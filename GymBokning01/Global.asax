c016c0dcea8d2d4a72697a8a11f135feef428c6a
PES <entermosjava@gmail.com>
2018-02-28 11:17:06 +0100

    Add project files.

---
+++ GymBokning01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GymBokning01.MvcApplication" Language="C#" %>
---
+++ GymBokning01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymBokning01.Startup))]
+namespace GymBokning01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
